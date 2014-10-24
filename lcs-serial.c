18033c67e387d9be66408c90a8fba2bf32d51071
ds49 <diogo.salgueiro@gmail.com>
2014-10-24 12:53:47 +0100

    Update
    Matrix read, output file creation

--- lcs-serial.c
+++ lcs-serial.c
@@ -3,20 +3,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define SIZE_BUFFER1 32
+#define SIZE_BUFFER1 2048
 
 #define max(a,b) ( ((a)>(b)) ? (a) : (b) )
 
  
 int main(){
 	
-	FILE * f;
+	FILE * f, *f_out;
 	char *buffer;
-	int size_x, size_y, i, j;
-	char *x=NULL, *y=NULL; 
+	int size_x, size_y, i, j, k;
+	char *x=NULL, *y=NULL, *z=NULL; 
 	int **c = NULL;
 	
-	f = fopen("ex10.15.in", "r");
+	f = fopen("ex150.200.in", "r"); //adicionar chamada atraves de argv[]
+	//adicionar verificacao para abertura correcta
 	
 	buffer = malloc(SIZE_BUFFER1*sizeof(char));
 	
@@ -50,6 +51,75 @@ int main(){
 		for(j=0;j<size_y;j++) printf("%d ", c[i][j]);
 		printf("\n");
 	}
+
+
+	//Leitura Matriz
+	//Criar sequencia de caracteres de saida, 
+	//quanto muito tera o tamanho da sequencia de entrada menor (se esta estiver totalmente repetida na maior)
+	//(Vejam se isto faz sentido...)
+
+	if(size_x<size_y)
+		z = malloc((size_x+1)*sizeof(char));
+	else
+		z = malloc((size_y+1)*sizeof(char));
+		
+	
+	printf("i=%d, j=%d\n", i-1, j-1); //debug
+	printf("valor ultimo elemento: %d\n", c[i-1][j-1]);//debug
+	
+	//usar os valores de "i-1" e "j-1" (ultima posicao da matriz)
+	i=i-1;
+	j=j-1;
+	
+	k=0; //(indice para o vector z)
+	
+	while((i>0)&&(j>0)){
+			
+			if (x[i]==y[j]) {	//match, copiar para z, avancar diagonal	
+				z[k]=x[i];
+				i=i-1;
+				j=j-1;
+				k++;
+			}
+			
+			else{
+				
+				if(c[i-1][j]>c[i][j-1])		//maior, esquerda ou acima?
+					i=i-1;		//move para cima
+				else
+					j=j-1;		//move para a esquerda
+			}
+		
+		printf("%d %d\n", i, j);
+	}
+	
+	//sai do ciclo na posicao a seguir ao ultimo caracter
+	z[k]='\0';		//terminar vector z
+	
+	printf("%s\n", z);
+	
+	//acho q e preciso inverter esta cena..
+	//mais vale escrever invertido para o ficheiro, percorre-se menos vezes os vectores gigantes
+	
+
+	//Escrita no ficheiro de saida
+	
+	f_out = fopen("ex150.200.out", "w"); //alterar para colocar o nome do ficheiro de entrada -".in" + ".out"
+	//verificar se abre correctamente
+	
+	//1a linha: tamanho da sequencia -> indice k (valor e o numero de cenas la dentro, pq esta uma posicao acima do ultimo caracter...)
+	fprintf(f_out, "%d\n", k);
+	
+	//2a linha: escrever o vector z (termina-lo em '\0' e lê-lo como string?)
+	
+	for(i=k-1;i>0;i--){
+		fprintf(f_out, "%c", z[i]);
+	}
+	fprintf(f_out, "%c", z[i]);
+	
+	//3a linha: \n
+	fprintf(f_out, "\n");
+
 
 	free(buffer);
 	free(x);
